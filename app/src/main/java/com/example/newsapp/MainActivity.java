c45544cb8dd00664c12bf4d681352f2535105030
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2020-11-16 01:22:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.newsapp.Model.Articles;
+import com.example.newsapp.Model.Headlines;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    final String API_KEY = "feb510c6f5c04eb790c6ba39c3be7854";
+    Adapter adapter;
+    List<Articles> articlesList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        String country = getCountry();
+        retrieveJson(country, API_KEY);
+    }
+
+    public void retrieveJson(String country, String apiKey) {
+
+        Call<Headlines> call = ApiClient.getInstance().getApi().getHeadlines(country, apiKey);
+        call.enqueue(new Callback<Headlines>() {
+            @Override
+            public void onResponse(Call<Headlines> call, Response<Headlines> response) {
+                if(response.isSuccessful() && response.body() != null) {
+                    articlesList.clear();
+                    articlesList = response.body().getArticles();
+                    adapter = new Adapter(MainActivity.this, articlesList);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Headlines> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public String getCountry() {
+        Locale locale = new Locale("en", "IN");
+        return locale.getCountry().toLowerCase();
+    }
+}
