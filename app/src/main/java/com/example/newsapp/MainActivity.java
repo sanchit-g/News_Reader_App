91181303180755f889447eb9cc17485023872e94
Sanchit Gupta <19ucc046@lnmiit.ac.in>
2020-11-17 17:09:15 +0530

    Added Swipe Refresh support

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.net.Uri;
 import android.os.Bundle;
@@ -22,6 +23,7 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements NewsItemClicked {
 
+    SwipeRefreshLayout swipeRefreshLayout;
     RecyclerView recyclerView;
     final String API_KEY = "feb510c6f5c04eb790c6ba39c3be7854";
     Adapter adapter;
@@ -32,19 +34,30 @@ public class MainActivity extends AppCompatActivity implements NewsItemClicked {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        swipeRefreshLayout = findViewById(R.id.swipeRefresh);
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         String country = getCountry();
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                retrieveJson(country, API_KEY);
+            }
+        });
+
         retrieveJson(country, API_KEY);
     }
 
     public void retrieveJson(String country, String apiKey) {
 
+        swipeRefreshLayout.setRefreshing(true);
         Call<Headlines> call = ApiClient.getInstance().getApi().getHeadlines(country, apiKey);
         call.enqueue(new Callback<Headlines>() {
             @Override
             public void onResponse(Call<Headlines> call, Response<Headlines> response) {
                 if(response.isSuccessful() && response.body() != null) {
+                    swipeRefreshLayout.setRefreshing(false);
                     articlesList.clear();
                     articlesList = response.body().getArticles();
                     adapter = new Adapter(MainActivity.this, articlesList, MainActivity.this);
@@ -54,6 +67,7 @@ public class MainActivity extends AppCompatActivity implements NewsItemClicked {
 
             @Override
             public void onFailure(Call<Headlines> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
                 Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
             }
         });
